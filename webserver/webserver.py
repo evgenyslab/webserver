857293a7503051205f5981366219d137cc01be38
evgenyslab <e.nuger@gmail.com>
2021-06-30 07:39:05 -0400

    cleanup

--- webserver/webserver.py
+++ webserver/webserver.py
@@ -2,89 +2,19 @@ import argparse
 import os
 import random
 import http.server
-from bs4 import BeautifulSoup
-from urllib import request, error
-import socket
 import socketserver
 import warnings
-
-"""
-Web Server python tool servers a directy
-by reading the index.html in the pack's web folder,
-parses the scripts, and inserts the scripts into a 
-single HTML file and servers the HTML to clients.
-
-A custom directory can be provided and the process
-repeats
-
-HOW TO PASS PARAMETERS TO WEBPAGE?
-
--> pass kwargs,
--> when parsing scripts, find matching 
-parameters and replace with kwards
-"""
-
-
-def checkExtension(file, extensions=()):
-    if len(extensions) == 0:
-        return True
-    else:
-        return file.endswith(extensions)
-
-
-def fileFinder(directory=[], extensions=(), walk=True):
-
-    if isinstance(directory, str):
-        directory = [directory]
-
-    files = []
-    paths = []
-    for k in directory:
-        if os.path.isfile(k) and checkExtension(k, extensions):
-            files.append(k)
-        elif os.path.isdir(k):
-            paths.append(k)
-
-    for k in paths:
-        if walk:
-            for dirpath, dirnames, filenames in os.walk(k):
-                for filename in filenames:
-                    if checkExtension(filename, extensions):
-                        files.append((dirpath, filename))
-        else:
-            for f in os.listdir(k):
-                if os.path.isfile(k + "/" + f) and checkExtension((k + "/" + f), extensions):
-                    files.append((k + "/", f))
-
-    return files
-
-
-def generateHandler(html):
-    """
-    Generates an http.server.BaseHTTPRequestHandler that is triggered on webrequest
-
-    :param html: path to root html file
-    :return:
-    """
-
-    class MyHandler(http.server.BaseHTTPRequestHandler):
-        def do_GET(self):
-            """Respond to a GET request."""
-            if self.path == '/':
-                self.send_response(200)
-                self.send_header("Content-type", "text/html")
-                self.end_headers()
-                self.wfile.write(html.encode())
-
-            else:
-                self.send_error(404)
-
-    return MyHandler
+from threading import Thread
 
 
 class Webserver:
     """
-
+    A class for serving a directory or an html string
+    
+    Input options:
+        dir
+        fileList
+        html string
     """
     def __init__(self, **kwargs):
         """!
@@ -92,107 +22,95 @@ class Webserver:
         @param host:
         @param port:
         """
-
+        self.__sample_html__ = '''
+                                <meta http-equiv="refresh" content="30" />
+                                <html>
+                                <head>
+                                <title>(Type a title for your page here)</title>
+                                <script type="text/javascript"> 
+                                function display_c(){
+                                var refresh=1000; // Refresh rate in milli seconds
+                                mytime=setTimeout('display_ct()',refresh)
+                                }
+                                
+                                function display_ct() {
+                                var x = new Date()
+                                document.getElementById('ct').innerHTML = x;
+                                display_c();
+                                 }
+                                </script>
+                                </head>
+                                
+                                <body onload=display_ct();>
+                                <span id='ct' ></span>
+                                
+                                </body>
+                                </html>
+                                '''
         # DEFAULTS
         self.host = "0.0.0.0"
-        self.sources = ""
-        self.port = 0
-        self.html = ""
+        self.port = 7777
+        self.html = None
+        self.directory = None
+        self.file_list = None
+        self.__serving_html__ = None
+        self.__parse_inputs__(**kwargs)
+        self.__decide_what_to_serve__()
         self.server = None
+        self.is_serving = False
+        self.__serving_thread__ = None
+        # TODO: resolve inputs
 
-        self.sourceDirectory = ""
-        self.sourceIndexFile = ""
-
-        if 'sources' not in kwargs:
-            raise(ValueError, "Input not provided")
-        if not (os.path.isfile(kwargs['sources']) or os.path.isdir(kwargs['sources'])):
-            raise(ValueError, "Error: input is neither a file or a directory")
-
-        if 'port' in kwargs:
-            self.port = kwargs['port']
-
-        if 'host' in kwargs:
-            # validate:
-            try:
-                socket.inet_aton(kwargs['host'])
-            except socket.error:
-                warnings.warn("Request Host at {:} is not available, reverting to default {:}".format(kwargs['host'],
-                                                                                                      self.host))
-
-        # parse sources:
-        # Single FILE:
-        if os.path.isfile(kwargs['sources']):
-            # validate is it html file:
-            if not kwargs['sources'].endswith('html'):
-                raise(ValueError, "input file {:} is not html".format(kwargs['sources']))
-            else:
-                self.sourceIndexFile = kwargs['sources']
-                self.sourceDirectory = '/'.join(self.sourceIndexFile.split('/')[:-1]) + '/'
-
-        # SERVE FOLDER
-        else:  # folder
-            self.sourceDirectory = kwargs['sources'] + '/' if kwargs['sources'][-1] != '/' else ''
-            self.sourceFiles = fileFinder(self.sourceDirectory)
-            # find index file:
-            fileList = [k[-1] for k in self.sourceFiles]
-            if fileList.count('index.html') == 0:
-                raise(ValueError, "No index.html file found in directory provided.")
-            elif fileList.count('index.html') > 1:
-                raise (ValueError, "More than one index.html file found in directory provided.")
-            else:
-                idx = fileList.index('index.html')
-                self.sourceIndexFile = ''.join(self.sourceFiles[idx])
-                # remove the file from source list:
-                self.sourceFiles.pop(idx)
-
-        self.parseIndexHTMLFile()
-        self.handler = generateHandler(self.html)
-
-
-
-    def parseIndexHTMLFile(self):
+    @staticmethod
+    def __generate_handle__(html=""):
         """
-        Read the sourceIndexFile
-        -> determine if it calls for scripts that need to be inserted & are available locally
-        -> determine if it calls for remote scripts (check if they can curl)
-        """
-        with open(self.sourceIndexFile, "r") as f:
-            doc = f.read()
-        soup = BeautifulSoup(doc, 'html.parser')
-        fileList = [k[-1] for k in self.sourceFiles]
-        scripts = soup.find_all('script')
-        # check each script...
-        for script in scripts:
-            # try this if HTTP
-            if script['src'].startswith('http'):
-                try:
-                    page = request.urlopen(script['src'])
-                    scriptRaw = page.read()
-                except error.URLError:
-                    warnings.warn("Could not reach {:}".format(script['src']))
-            else:
-                # local file
-                if script['src'] in fileList:
-                    idx = fileList.index(script['src'])
-                    with open(''.join(self.sourceFiles[idx]), "r") as f:
-                        scriptRaw = f.read()
-                else:
-                    pass
-                script.string = scriptRaw  # raw js UTF-8
-                del(script['src'])  # remove tag
+        Generates an http.server.BaseHTTPRequestHandler that is triggered on webrequest
 
-        # now, convert the soup back to byte-doc
-        self.html = str(soup)  # and serve it :)
-
-    def serve(self):
-        """
-
+        :param html: path to root html file
         :return:
         """
+
+        class MyHandler(http.server.BaseHTTPRequestHandler):
+            def do_GET(self):
+                """Respond to a GET request."""
+                if self.path == '/':
+                    self.send_response(200)
+                    self.send_header("Content-type", "text/html")
+                    self.end_headers()
+                    self.wfile.write(html.encode())
+
+                else:
+                    self.send_error(404)
+
+        return MyHandler
+
+    def __parse_inputs__(self, **kwargs):
+        self.host = kwargs.get('host', self.host)
+        self.port = kwargs.get('port', self.port)
+        self.html = kwargs.get('html', self.html)
+        self.directory = kwargs.get('directory', self.directory)
+        self.file_list = kwargs.get('file_list', self.file_list)
+
+    def __decide_what_to_serve__(self):
+        """
+        Decides on what inputs to serve by priority:
+            Directory
+            File List
+            HTML
+        """
+        # check if self.dir or self.fileList, default to self.html if none of the previous
+        # successfully parsed
+        if self.html is not None:
+            self.__serving_html__ = self.html
+        else:
+            self.__serving_html__ = self.__sample_html__
+
+    def __serve_with_thread__(self, host, port, html):
+        # TODO: auto inject refresh line '<meta http-equiv="refresh" content="30" />'
         bound = False
-        port = self.port
+        handler = self.__generate_handle__(html)
         while not bound:
-            self.server = socketserver.TCPServer(("", port), self.handler, bind_and_activate=False)
+            self.server = socketserver.TCPServer(("", port), handler, bind_and_activate=False)
             self.server.allow_reuse_address = True
             try:
                 self.server.server_bind()
@@ -200,40 +118,81 @@ class Webserver:
                 bound = True
             except OSError:
                 # could not bind; maybe, choose a random port then
-                if port == self.port:
-                    warnings.warn("Port {:} in use".format(self.port))
+                warnings.warn("Port {:} in use".format(port))
                 self.server.server_close()
                 port = random.randrange(9999, 13000)
 
         # Star the server
-        print("Starting web serving at: http://{:s}:{:d}\n\n".format(self.host, port))
+        print("Starting web serving at: http://{:s}:{:d}\n\n".format(host, port))
         self.server.serve_forever()
 
+    def start(self):
+        """
 
-if __name__ == "__main__":
-    # input args needs to be an index.html file or directory
-    parser = argparse.ArgumentParser(description='Simple Web Server')
+        :return:
+        """
+        if self.is_serving:
+            return
+        if self.__serving_html__ is None:
+            return
+        self.__serving_thread__ = Thread(target=self.__serve_with_thread__, args=(self.host, self.port, self.__serving_html__, ))
+        self.__serving_thread__.start()
+        self.is_serving = True
 
-    parser.add_argument("-i",
-                        dest="sources",
-                        required=True,
-                        help="Web Source Location")
+    def __del__(self):
+        self.stop()
 
-    parser.add_argument("--host",
-                        dest="hostAddress",
-                        default="0.0.0.0")
+    def stop(self):
+        if not self.is_serving:
+            return
 
-    parser.add_argument("--port",
-                        dest="port",
-                        default="12345",
-                        type=int)
+        def stop_server():
+            self.server.shutdown()
 
-    parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+        __thread__ = Thread(target=stop_server)
+        __thread__.start()
+        __thread__.join()
+        self.__serving_thread__.join()
+        self.is_serving = False
+        print("stopped")
 
-    args = parser.parse_args()
+    def serve(self, **kwargs):
+        """
+        Generic server/re-serve
+        html
+        port
+        host
+        """
+        self.stop()
+        self.__parse_inputs__(**kwargs)
+        self.__decide_what_to_serve__()
+        self.start()
 
-    # args.default should be an index.html
-    server = Webserver(sources=args.sources,
-                       host=args.hostAddress,
-                       port=args.port)
-    server.serve()
+#
+# if __name__ == "__main__":
+#     # input args needs to be an index.html file or directory
+#     parser = argparse.ArgumentParser(description='Simple Web Server')
+#
+#     parser.add_argument("-i",
+#                         dest="sources",
+#                         required=True,
+#                         help="Web Source Location")
+#
+#     parser.add_argument("--host",
+#                         dest="hostAddress",
+#                         default="0.0.0.0")
+#
+#     parser.add_argument("--port",
+#                         dest="port",
+#                         default="12345",
+#                         type=int)
+#
+#     parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+#
+#     args = parser.parse_args()
+#
+#     # args.default should be an index.html
+#     server = Webserver(sources=args.sources,
+#                        host=args.hostAddress,
+#                        port=args.port)
+#     server.serve()
